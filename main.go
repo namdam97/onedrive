8cce2604d9f5183d90e229e33cebde57a4eeb923
namdt <namdamthuan@vccorp.vn>
2023-09-29 10:09:31 +0700

    them api upload file. tu dong xac thuc

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"time"
@@ -19,9 +20,9 @@ import (
 // AuthURL:  "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/oauth2/authorize",
 // TokenURL: "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/oauth2/token",
 
-func main() {
+var (
 	// Khai báo thông tin xác thực OAuth2
-	config := oauth2.Config{
+	config = oauth2.Config{
 		ClientID:     "e240abd3-62fa-4378-ae24-9e92f078fc63",
 		ClientSecret: "ZSs8Q~nT6zvm19CIE72shXSp-sHelDFYSsz43cBJ",
 		RedirectURL:  "http://localhost:9092/oauth2callback",
@@ -31,6 +32,62 @@ func main() {
 		},
 		Scopes: []string{"files.readwrite.all"},
 	}
+	handler *multipart.FileHeader
+)
+
+func startOAuthFlow() {
+	// Tạo URL xác thực và chuyển hướng người dùng đến trang đăng nhập
+	authURL := config.AuthCodeURL("", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser:\n%v\n", authURL)
+	// Mở trình duyệt tự động với URL xác thực
+	if err := open.Run(authURL); err != nil {
+		log.Fatalf("Failed to open browser: %v", err)
+	}
+
+	// Đợi cho phép người dùng đăng nhập
+	fmt.Println("Waiting for authorization...")
+
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("Authorization process complete.")
+}
+
+func uploadHttpFile(w http.ResponseWriter, r *http.Request) {
+	// Parse the request to get the file
+	err := r.ParseMultipartForm(10 << 20) // 10 MB limit
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	// Get the file from the form data
+	file, handlerFlie, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Unable to get file from form", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+	handler = handlerFlie
+
+	// Create a new file on the server to store the uploaded file
+	f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		http.Error(w, "Unable to create file on server", http.StatusInternalServerError)
+		return
+	}
+	// Copy the file from the request to the server file
+	_, err = io.Copy(f, file)
+	if err != nil {
+		http.Error(w, "Unable to copy file", http.StatusInternalServerError)
+		return
+	}
+	go startOAuthFlow()
+	fmt.Fprintf(w, "File uploaded successfully: %s", handler.Filename)
+}
+
+func main() {
+	// Set up an HTTP server to handle file uploads
+	http.HandleFunc("/upload", uploadHttpFile)
 
 	// Tạo HTTP server để lắng nghe callback từ OneDrive
 	http.HandleFunc("/oauth2callback", func(w http.ResponseWriter, r *http.Request) {
@@ -51,29 +108,15 @@ func main() {
 	// Khởi động HTTP server
 	go http.ListenAndServe(":9092", nil)
 
-	// Tạo URL xác thực và chuyển hướng người dùng đến trang đăng nhập
-	authURL := config.AuthCodeURL("", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser:\n%v\n", authURL)
-	// Mở trình duyệt tự động với URL xác thực
-	if err := open.Run(authURL); err != nil {
-		log.Fatalf("Failed to open browser: %v", err)
-	}
-
-	// Đợi cho phép người dùng đăng nhập
-	fmt.Println("Waiting for authorization...")
-
-	time.Sleep(1 * time.Second)
-
-	fmt.Println("Authorization process complete.")
 	select {}
 }
 
 // /me/drive/items/{item-id}/content
 // me/drive/root:/testhodo/test2.txt:/content
 func uploadFile(accessToken string) {
+	filePath := handler.Filename
 	// Tạo yêu cầu HTTP để tải tệp lên OneDrive
-	uploadURL := "https://graph.microsoft.com/v1.0/me/drive/root:/testhodo/hung.pptx:/content"
-	filePath := "testhodo/hung.pptx"
+	uploadURL := fmt.Sprintf("https://graph.microsoft.com/v1.0/me/drive/root:/testhodo/%s:/content", filePath)
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatalf("Failed to open file: %v", err)
@@ -130,8 +173,9 @@ func uploadFile(accessToken string) {
 }
 
 func getInfoFile(accessToken string) {
+	filePath := handler.Filename
 	// Tạo yêu cầu HTTP để tải tệp lên OneDrive
-	url := "https://graph.microsoft.com/v1.0/me/drive/root:/testhodo/hung.pptx"
+	url := "https://graph.microsoft.com/v1.0/me/drive/root:/testhodo/" + filePath
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
